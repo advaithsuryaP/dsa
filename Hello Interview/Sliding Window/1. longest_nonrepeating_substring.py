4ae5b712a5fb47a8dfd2625cc06ddd0b09c7d0c1
advaithsuryaP <padvaithsurya@gmail.com>
2026-01-05 23:40:16 -0500

    Refactor "Longest Non-Repeating Substring" implementation to enhance clarity and efficiency; streamline variable initialization and improve readability. Add additional example test cases for comprehensive validation.

--- Hello Interview/Sliding Window/1. longest_nonrepeating_substring.py
+++ Hello Interview/Sliding Window/1. longest_nonrepeating_substring.py
@@ -13,22 +13,19 @@ Explanation: The longest substring without repeating characters is "egh".
 '''
 
 def longest_nonrepeating_substring(s: str) -> int:
-    left: int = 0
-
     result: int = 0
 
-    window_size: int = 0
-    unique: set[int] = set()
+    left: int = 0
+    unique: set[int] = set[int]()
 
     for right in range(len(s)):
         while s[right] in unique:
             unique.remove(s[left])
             left += 1
-        
+ 
         unique.add(s[right])
 
-        window_size = right - left + 1
-        result = max(result, window_size)
+        result = max(result, right - left + 1)
 
     return result
 
@@ -41,6 +38,6 @@ def longest_nonrepeating_substring(s: str) -> int:
 
 
 print(longest_nonrepeating_substring("abcabcbb")) # 3
-# print(longest_nonrepeating_substring("eghghhgg")) # 3 
-# print(longest_nonrepeating_substring("aabbcc")) # 2
-# print(longest_nonrepeating_substring("substring")) # 8
+print(longest_nonrepeating_substring("eghghhgg")) # 3 
+print(longest_nonrepeating_substring("aabbcc")) # 2
+print(longest_nonrepeating_substring("substring")) # 8
