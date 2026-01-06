70fbe04a4138e7294a1005f848492083e4e86f50
advaithsuryaP <padvaithsurya@gmail.com>
2026-01-06 03:55:52 -0500

    Refactor "Character Replacement" function to improve logic for tracking character occurrences and window size; enhance readability with clearer variable names and additional debug statements for better insight into the algorithm's execution.

--- Hello Interview/Sliding Window/2. character_replacement.py
+++ Hello Interview/Sliding Window/2. character_replacement.py
@@ -13,18 +13,42 @@ Explanation: Replace the first 'A' and 'C' with 'B' to form "BBBBBCDD". The long
 '''
 
 def character_replacement(s: str, k: int) -> int:
-    result: int = 0
+    result: float = float('-inf')
 
     left: int = 0
-    occurences: dict[str, int] = dict()
-
-    for right in range(len(s)):
-        # while s[right] not in occurences and k == 0:
-        #     continue
-
-        occurences[s[right]] = occurences.get(s[right], 0) + 1
-        # count 
     
+    occurrences: dict[str, int] = dict[str, int]()
 
+    max_frequency: int = 0
+    window_size: int = 0
+    for right in range(len(s)):
+        print(f"----------- Iterating {s[right]} -----------")
+        
+        
+        occurrences[s[right]] = occurrences.get(s[right], 0) + 1
+
+        while window_size - max_frequency > k:
+            # Window is invalid here
+            occurrences[s[left]] -= 1
+            print(f"Invalid window. Decremented the count of {s[left]} in {occurrences}")
+            left += 1
+            window_size = right - left + 1
+            max_frequency_key: str = max(occurrences, key=occurrences.get)
+            max_frequency = occurrences[max_frequency_key]
+
+
+        max_frequency_key: str = max(occurrences, key=occurrences.get)
+        max_frequency = occurrences[max_frequency_key]
+        
+        window_size = right - left + 1
+        result = max(result, window_size)
+        
+        
+        print(f"Window Size: {window_size}, Max Frequency: {max_frequency}, Occurrences: {occurrences} || Result: {result}")
+
+
+
+    return result
+        
 
 print(character_replacement("BBABCCDD", 2)) # 5
