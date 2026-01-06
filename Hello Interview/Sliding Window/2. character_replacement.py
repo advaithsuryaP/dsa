3ee6ee039bf13fb44d34f30345e3a7020f987c9c
advaithsuryaP <padvaithsurya@gmail.com>
2026-01-05 23:42:11 -0500

    Add initial implementation for the "Character Replacement" problem; create a function to find the length of the longest substring with identical letters after at most k replacements. The commit includes a detailed problem statement and example usage.

---
+++ Hello Interview/Sliding Window/2. character_replacement.py
@@ -0,0 +1,18 @@
+'''
+Problem: 
+
+Write a function to find the length of the longest substring containing the same letter in a given string s, 
+after performing at most k operations in which you can choose any character of the string and change it to any other uppercase English letter.
+
+Example:
+s = "BBABCCDD"
+k = 2
+
+Output: 5
+Explanation: Replace the first 'A' and 'C' with 'B' to form "BBBBBCDD". The longest substring with identical letters is "BBBBB", which has a length of 5.
+'''
+
+def character_replacement(s: str, k: int) -> int:
+    return
+
+print(character_replacement("BBABCCDD", 2)) # 5
