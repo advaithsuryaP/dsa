b49ae03004cf6ed4ec4eb48ce7d0df966ee4678e
advaithsuryaP <padvaithsurya@gmail.com>
2026-01-07 23:59:07 -0500

    Add initial implementation of the `fruit_basket` function in "4. fruit-basket.py"; includes a basic structure for calculating the maximum number of fruits collected from an array, along with an example usage for demonstration.

--- Hello Interview/Sliding Window/4. fruit-basket.py
+++ Hello Interview/Sliding Window/4. fruit-basket.py
@@ -4,3 +4,10 @@ Write a function to calculate the maximum number of fruits you can collect from 
 You can start collecting fruits from any position in the array, but you must stop once you encounter a third distinct type of fruit.
 The goal is to find the longest subarray where at most two different types of fruits are collected.
 '''
+
+def fruit_basket(fruits: List[int]) -> int:
+    result: int = 0
+    return
+
+
+print(fruit_basket([1, 2, 1, 3, 4, 3, 5, 1, 2])) # 3
