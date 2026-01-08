9d001314df525ba0a2b07aeaa973f5d717cb2902
advaithsuryaP <padvaithsurya@gmail.com>
2026-01-07 23:58:39 -0500

    Add detailed problem statement in "4. fruit-basket.py" to clarify the sliding window challenge of collecting fruits; outlines the goal of finding the longest subarray with at most two distinct fruit types.

--- Hello Interview/Sliding Window/4. fruit-basket.py
+++ Hello Interview/Sliding Window/4. fruit-basket.py
@@ -1,3 +1,6 @@
 '''
 Problem:
+Write a function to calculate the maximum number of fruits you can collect from an integer array fruits, where each element represents a type of fruit. 
+You can start collecting fruits from any position in the array, but you must stop once you encounter a third distinct type of fruit.
+The goal is to find the longest subarray where at most two different types of fruits are collected.
 '''
